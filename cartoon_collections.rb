e50a8fd8dcc8dc55e0abe1f162c5914e784b4469
John Grubbs <johnmlowery@gmail.com>
2018-08-21 17:07:34 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,8 @@ def roll_call_dwarves(array)
 
 
 def summon_captain_planet(array)
-  array.map! {|word| word.capitalize}
-  array.each {|word| puts word + '!'}
+  array.collect do |word|
+    puts "#{word.capitalize}!"
 end
 
 def long_planeteer_calls# code an argument here
