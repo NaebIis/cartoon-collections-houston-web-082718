ac7433419b1a0ecaab5821cf7b0be93d180b11d1
John Grubbs <johnmlowery@gmail.com>
2018-08-21 16:51:18 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(array)
   array.each_with_index {|name, index| puts "#{index + 1}. 
     #{name}"}
   end
-end
+
 
 def summon_captain_planet# code an argument here
   # Your code here
